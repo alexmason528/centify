b19b264a9ea2f99b7b447cc352ca2e00bcda8fa6
FantasticBoy <fantasticboy@FantasticBoys-Mac.local>
2016-07-21 09:44:15 +0200

    jwt attached

--- src/utils/AuthService.js
+++ src/utils/AuthService.js
@@ -1,40 +1,75 @@
+import { EventEmitter } from 'events'
+import { isTokenExpired } from './jwtHelper'
 import Auth0Lock from 'auth0-lock'
 
-export default class AuthService {
-	constructor(clientId, domain) {
-		
-		// Configure Auth0
-		this.lock = new Auth0Lock(clientId, domain, {})
+export default class AuthService extends EventEmitter {
+  constructor(clientId, domain) {
+    super()
+    // Configure Auth0
+    this.lock = new Auth0Lock(clientId, domain, {})
+    // Add callback for lock `authenticated` event
+    this.lock.on('authenticated', this._doAuthentication.bind(this))
+    // Add callback for lock `authorization_error` event
+    this.lock.on('authorization_error', this._authorizationError.bind(this))
+    // binds login functions to keep this context
+    this.login = this.login.bind(this)
+  }
 
-		// Add callback for lock 'authenticated' event
-		this.lock.on('authenticated', this._doAuthentication.bind(this))
+  _doAuthentication(authResult){
+    // Saves the user token
+    this.setToken(authResult.idToken)
+    // Async loads the user profile data
+    this.lock.getProfile(authResult.idToken, (error, profile) => {
+      if (error) {
+        console.log('Error loading the Profile', error)
+      } else {
+        this.setProfile(profile)
+      }
+    })
+  }
 
-		// binds login functions to keep this context
-		this.login = this.login.bind(this)
-	}
+  _authorizationError(error){
+    // Unexpected authentication error
+    console.log('Authentication Error', error)
+  }
 
-	_doAuthentication(authResult) {
-		this.setToken(authResult.idToken)
-	}
+  login() {
+    // Call the show method to display the widget.
+    this.lock.show()
+  }
 
-	login() {
-		this.lock.show()
-	}
+  loggedIn(){
+    // Checks if there is a saved token and it's still valid
+    const token = this.getToken()
+    return !!token && !isTokenExpired(token)
+  }
 
-	loggedIn() {
-		return !!this.getToken()
-	}
+  setProfile(profile){
+    // Saves profile data to localStorage
+    localStorage.setItem('profile', JSON.stringify(profile))
+    // Triggers profile_updated event to update the UI
+    this.emit('profile_updated', profile)
+  }
 
-	setToken(idToken) {
-		localStorage.setItem('id_token', idToken)
-	}
+  getProfile(){
+    // Retrieves the profile data from localStorage
+    const profile = localStorage.getItem('profile')
+    return profile ? JSON.parse(localStorage.profile) : {}
+  }
 
-	getToken() {
-		return localStorage.getItem('id_token')
-	}
+  setToken(idToken){
+    // Saves user token to localStorage
+    localStorage.setItem('id_token', idToken)
+  }
 
-	logout() {
-		localStorage.removeItem('id_token');
-	}
+  getToken(){
+    // Retrieves the user token from localStorage
+    return localStorage.getItem('id_token')
+  }
 
+  logout(){
+    // Clear user token and profile data from localStorage
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('profile');
+  }
 }
