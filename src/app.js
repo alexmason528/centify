6180dca21ac86b1d8c4260b2c261c150d28226a0
Miroslav Stefan <stefan6@Miroslavs-Mac-mini.local>
2016-07-27 16:37:38 +0200

    Redux integrating

--- src/app.js
+++ src/app.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import ReactDOM from 'react-dom'
+import { Router, hashHistory } from 'react-router'
+import { Provider } from 'react-redux'
+import { syncHistoryWithStore } from 'react-router-redux';
+
+import createStore from 'redux/create';
+import ApiClient from 'utils/ApiClient';
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'font-awesome/css/font-awesome.css'
 import '@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css'
 import './app.css'
 
-import App from 'containers/App/App'
-
-import {hashHistory} from 'react-router'
 import makeRoutes from './routes'
 
+const client = new ApiClient();
+const store = createStore(hashHistory, client, window.__data);
+const history = syncHistoryWithStore(hashHistory, store);
 const routes = makeRoutes()
 
 const mountNode = document.querySelector('#root');
+
 ReactDOM.render(
-  <App history={hashHistory}
-        routes={routes} />,
+  <Provider store={store} key="provider">
+    <Router history={history}>
+      {routes}
+    </Router>
+  </Provider>,
 mountNode);
