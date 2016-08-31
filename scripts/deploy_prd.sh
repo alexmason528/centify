3593dd8d859bbdcc12714834a777cd95a7773c4b
Matthew Horoschun <matthew@centify.com>
2016-08-31 22:36:11 +1000

    Update deploy so that all files have 1 hour Cache-Control except index which is 60 seconds. Bumping version will create new .js/.css file names so will be quick to switch across.

--- scripts/deploy_prd.sh
+++ scripts/deploy_prd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cd dist && aws s3 cp . s3://my.centify.com/ --exclude "*" --include "*.png" --include "*.js" --include "*.html" --include "*.css" --include "*.svg" --recursive --cache-control 3600
+cd dist
+aws s3 cp . s3://my.centify.com/ --exclude "*" --include "*.png" --include "*.js" --include "*.html" --include "*.css" --include "*.svg" --exclude "index.html" --recursive --cache-control 3600
+aws s3 cp . s3://my.centify.com/ --exclude "*" --include "index.html" --cache-control 60
 aws cloudfront create-invalidation --distribution-id E1RG64IKGU4Q4A --paths "/*"
 
--- scripts/deploy_stg.sh
+++ scripts/deploy_stg.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-cd dist && aws s3 cp . s3://staging-my.centify.com/ --exclude "*" --include "*.png" --include "*.js" --include "*.html" --include "*.css" --include "*.svg" --recursive --cache-control 3600
+cd dist
+aws s3 cp . s3://staging-my.centify.com/ --exclude "*" --include "*.png" --include "*.js" --include "*.html" --include "*.css" --include "*.svg" --exclude "index.html" --recursive --cache-control 3600
+aws s3 cp . s3://staging-my.centify.com/ --exclude "*" --include "index.html" --cache-control 60
 aws cloudfront create-invalidation --distribution-id ES821UED2LALD --paths "/*"
