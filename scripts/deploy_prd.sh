09d70be5723d84e0597070db298f6ca50ebd3b56
Matthew Horoschun <matthew@centify.com>
2016-08-31 21:43:30 +1000

    Set Cache-Control to 1 hour (3600) but create an invalidate which flushes all edges when deploying

--- scripts/deploy_prd.sh
+++ scripts/deploy_prd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cd dist && aws s3 cp . s3://my.centify.com/ --exclude "*" --include "*.png" --include "*.js" --include "*.html" --include "*.css" --include "*.svg" --recursive --cache-control 600
+cd dist && aws s3 cp . s3://my.centify.com/ --exclude "*" --include "*.png" --include "*.js" --include "*.html" --include "*.css" --include "*.svg" --recursive --cache-control 3600
+aws cloudfront create-invalidation --distribution-id E1RG64IKGU4Q4A --paths "/*"
 
--- scripts/deploy_stg.sh
+++ scripts/deploy_stg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd dist && aws s3 cp . s3://staging-my.centify.com/ --exclude "*" --include "*.png" --include "*.js" --include "*.html" --include "*.css" --include "*.svg" --recursive --cache-control 600
-
+cd dist && aws s3 cp . s3://staging-my.centify.com/ --exclude "*" --include "*.png" --include "*.js" --include "*.html" --include "*.css" --include "*.svg" --recursive --cache-control 3600
+aws cloudfront create-invalidation --distribution-id ES821UED2LALD --paths "/*"
