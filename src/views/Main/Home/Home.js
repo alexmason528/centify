392d442bf6e3ebb7a064aa661461245ba374d1d7
Alex Mason <alexmason528@hotmail.com>
2016-09-21 11:00:06 -0400

    Update Content

--- src/views/Main/Home/Home.js
+++ src/views/Main/Home/Home.js
@@ -15,46 +15,45 @@ const Home = () => {
         </div>
         
         <div className="slds-m-vertical--x-large">
-          <h3 className="slds-text-title--caps" style={{ fontSize: 18, fontWeight: 600 }}>GAME TYPES</h3>
-          <div className="slds-m-bottom--large"> The Centify Game Types are used to encourage positive behaviors in sales and service people. These behaviors are usually related to activities or tasks they should complete as part of their job and the closing of sales or cases (for service people). The Games encourage individuals or teams and can promote collaboration amongst a team of people to raise the performance levels of all staff.
+          <h3 className="slds-text-title--caps" style={{ fontSize: 18, fontWeight: 600 }}>COMPETITIONS</h3>
+          <div className="slds-m-bottom--large"> The Centify Competitions are used to encourage positive behaviors in sales and service people. These behaviors are usually related to activities or tasks they should complete as part of their job and the closing of sales or cases (for service people). The Competitions encourage individuals or teams and can promote collaboration amongst a team of people to raise the performance levels of all staff.
           <br/><br/>
-          1. Over the line<br/>
-          2. The Race<br/>
-          3. Tug of War<br/>
-          4. Countdown
+          1. Collaborate<br/>
+          2. Places<br/>
+          3. Eliminate<br/>
+          4. Time
           </div>
         </div>
 
         <div className="slds-m-vertical--x-large">
-          <h3 className="slds-text-title--caps" style={{ fontSize: 18, fontWeight: 600 }}>Over the line</h3>
-          <div className="slds-m-bottom--large">Has multiple participants and they can be individuals or teams. There are two ways an Over the Line finishes:  
-            <br/>1. When the first individual or Team (aggregate score of all team members) reaches the target.
-            <br/>2. When all individual participants or all Team members reach the target.
+          <h3 className="slds-text-title--caps" style={{ fontSize: 18, fontWeight: 600 }}>Collaborate</h3>
+          <div className="slds-m-bottom--large">Has multiple participants and they can be individuals or teams. The goal of the collaboration is to get everyone to hit a target.  The competition doesn't end until everyone is done if someone doesn't make it, no one gets rewarded.  The goal is to encourage collaboration between team members and get everyone cheering together!
           </div>
         </div>
 
           
         <div className="slds-m-vertical--x-large">
-          <h3 className="slds-text-title--caps" style={{ fontSize: 18, fontWeight: 600 }}>The Race</h3>
-          <div className="slds-m-bottom--large">Has multiple individual participants. The Race finishes when the winners (usually First, Second and Third ) reach the target. The Sales Manager or Game Administrator can set up how many placed winners there are and if the is for individuals or teams.
+          <h3 className="slds-text-title--caps" style={{ fontSize: 18, fontWeight: 600 }}>Places</h3>
+          <div className="slds-m-bottom--large">Has multiple individual participants. Places finishes when the winners, usually First, Second and Third but that's up to you, reach the target. The Sales Manager or Game Administrator can set up how many placed winners there are and if the is for individuals or teams.
           </div>
         </div>
 
         <div className="slds-m-vertical--x-large">
-          <h3 className="slds-text-title--caps" style={{ fontSize: 18, fontWeight: 600 }}>Tug of War</h3>
-          <div className="slds-m-bottom--large">The Tug of War is between two Teams. The Game finishes when one team reaches or exceeds the target measure and there is a specified Gap between the score of the two Teams.
+          <h3 className="slds-text-title--caps" style={{ fontSize: 18, fontWeight: 600 }}>Eliminate</h3>
+          <div className="slds-m-bottom--large">Is an elimination game that focuses on poor performers within the Game criteria. At random intervals spread across the game the person in last place is eliminated. The competition ends when there is only one participant left. An Elimination competition may have a minimum threshold target that the winner must achieve in order to receive the reward. 
+          </div>
+        </div>
+
+        <div className="slds-m-vertical--x-large">
+          <h3 className="slds-text-title--caps" style={{ fontSize: 18, fontWeight: 600 }}>Time</h3>
+          <div className="slds-m-bottom--large">Is an individual participant or team competition where the goal is to hit a target in the given time.  The focus is on your own targets and doesn't require everyone to get across the line to win.   
           </div>
         </div>
         
-        <div className="slds-m-vertical--x-large">
-          <h3 className="slds-text-title--caps" style={{ fontSize: 18, fontWeight: 600 }}>Countdown</h3>
-          <div className="slds-m-bottom--large">Is an elimination game that focuses on poor performers within the Game criteria. At random intervals spread the person in last place is eliminated.Countdown ends when there is only one participant left. A Countdown Game may have a minimum threshold target that the winner must achieve in order to receive the reward. A Countdown may provide a Second Chance Life to a participant who has been eliminated.
-          </div>
-        </div>
 
         <div className="slds-m-vertical--x-large">
-          <h3 className="slds-text-title--caps" style={{ fontSize: 18, fontWeight: 600 }}>GAME THEMES</h3>
-          <div className="slds-m-bottom--large"> We offer many game themes, such as:<br/>
+          <h3 className="slds-text-title--caps" style={{ fontSize: 18, fontWeight: 600 }}>GAMES</h3>
+          <div className="slds-m-bottom--large"> We offer many games, such as:<br/>
             <br/>
             Race to the Moon - This is a Race Game game with the number of positions defined by the Sales Manager. You would normally have three places with different rewards for each placing. This game is most likely to be used to increase sales closed rates at month/quarter or year end. It is also applicable to lifting activities amongst reps mid month in order to drive the likelihood of a sale by month end. 
             <br/><br/>
